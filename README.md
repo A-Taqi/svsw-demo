442f5002f2cd7daab540c2b31e9b08855a336509
A-Taqi <a2taqi@gmail.com>
2024-08-11 21:04:08 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Stateless vs Stateful Widgets - DEMO (CODED)
+
+This is a demo for Coded.
+
+## Setup Instructions
+
+1. [Set up an editor](https://docs.flutter.dev/get-started/editor?tab=vscode) (This project was done with **VS Code**).
+2. In VS Code, go to **Run** > **Start Debugging**
+3. Choose **Chrome** or your favorite browser
+4. Wait to see the project's main page which should say *Welcome to the Demo!*
+
+## Demo Objectives
+
+The objective of this Demo is to demonstrate the usage of Stateless vs Stateful Widgets. To do that,
+we will be making a simple form where the user will enter their name in an input field, click a button
+and be greeted personally by the app. Let's get cooking 🍳!
+
+1. Create a ***Stateful*** Widget which will hold our form.
+2. Create a ***Stateful*** Widget which will hold our input field.
+2. Create a ***Stateless*** Widget which will display our personalized greeting text.
+3. Create a ***Stateless*** Widget which will hold our button to submit the name to the greeting text.
+
+## Note
+The folder `lib/widgets/` has been conveniently created for you should you wish to separate your widgets there.
+
+## Tips
+In **VS Code**, you have the following shortcuts that can come in handy:
+* Typing `stless` in a file will automatically insert a `StatelessWidget`
+* Typing `stful` in a file will automatically insert a `StatefulWidget`
---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CodedDemoApp());
+}
+
+class CodedDemoApp extends StatelessWidget {
+  const CodedDemoApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Stateless vs Stateful Widgets Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+        useMaterial3: true,
+      ),
+      home: const DemoMainPage(title: 'Stateless vs Stateful Widgets Demo'),
+    );
+  }
+}
+
+class DemoMainPage extends StatefulWidget {
+  const DemoMainPage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<DemoMainPage> createState() => _DemoMainPageState();
+}
+
+class _DemoMainPageState extends State<DemoMainPage> {
+  // Here you can define your state variables and functions.
+  // Make sure to call setState() whenever you want to update the UI.
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: const Padding(
+          padding: EdgeInsets.all(200.0),
+          child: Center(
+            // Center is a layout widget. It takes a single child and positions it
+            // in the middle of the parent.
+            child: Column(
+              // Column is also a layout widget. It takes a list of children and
+              // arranges them vertically. By default, it sizes itself to fit its
+              // children horizontally, and tries to be as tall as its parent.
+              //
+              // Column has various properties to control how it sizes itself and
+              // how it positions its children. Here we use mainAxisAlignment to
+              // center the children vertically; the main axis here is the vertical
+              // axis because Columns are vertical (the cross axis would be
+              // horizontal).
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'Welcome to the Demo!',
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+}
